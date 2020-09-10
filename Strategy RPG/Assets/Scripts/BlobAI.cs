2229125c42a122c736451c0fd3a263d7f89b438d
Scaredturtle <demarcam2012@yahoo.com>
2020-09-10 01:43:27 -0400

    Movement refined and collision

---
+++ Strategy RPG/Assets/Scripts/BlobAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobAI : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Random.Range(-1, 2);
+        movement.y = Random.Range(-1, 2);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
--- Strategy RPG/Assets/Scripts/DudeMovement.cs
+++ Strategy RPG/Assets/Scripts/DudeMovement.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class DudeMovement : MonoBehaviour
 {
-    public float speed = 1;                //Floating point variable to store the player's movement speed.
+    public float speed = 1f;                //Floating point variable to store the player's movement speed.
 
-    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    Vector2 movement;
 
     // Use this for initialization
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rb2d = GetComponent<Rigidbody2D>();
+        //rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
+        rb2d.MovePosition(rb2d.position + movement * speed * Time.fixedDeltaTime);
+
+        /*
+
+        Trying a modified verion of this code which might stop the sliding. Waiting for Taylor's input before deciding how to handle movement.
+
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
 
@@ -29,5 +45,6 @@ public class DudeMovement : MonoBehaviour
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
+        */
     }
 }
