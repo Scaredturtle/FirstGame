3fefa3717a16da071b207c244d792ad1dbcde5e5
Mr. Fix-it <demarchet@yahoo.com>
2020-10-14 01:21:05 -0400

    Added OtherDude and movement
    Made a movement script that moves the character exactly 32 (pixels?) before allowing new input and will stop if no further movement key is pressed.
    
    I have to figure out how to limit diagonal movement.

---
+++ Strategy RPG/Assets/Scripts/OtherDudeMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherDudeMove : MonoBehaviour
+{
+    public float speed = 1f;                //Floating point variable to store the player's movement speed.
+
+    public Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    Vector2 movement;
+    bool ismoving;
+    int countDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDistance = 0;
+        ismoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for previous input, if not currently moving, allow another move
+        if(ismoving == false)
+        {
+            //Move Up
+            if(Input.GetKey("w"))
+            {
+                movement.y = 1;
+                ismoving = true;
+            }
+            //Move Down
+            if(Input.GetKey("s"))
+            {
+                movement.y = -1;
+                ismoving = true;
+            }
+            //Move Right
+            if(Input.GetKey("d"))
+            {
+                movement.x = 1;
+                ismoving = true;
+            }
+            //Move Left
+            if(Input.GetKey("a"))
+            {
+                movement.x = -1;
+                ismoving = true;
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        if(ismoving == true)
+        {
+            rb2d.MovePosition(rb2d.position + movement * speed * Time.fixedDeltaTime);
+            countDistance = countDistance +1;
+            if(countDistance>32)
+            {
+                ismoving = false;
+                countDistance = 0;
+                movement.x = 0;
+                movement.y = 0;
+            }
+        }
+    }
+}
